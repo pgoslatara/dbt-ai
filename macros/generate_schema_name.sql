a4969cef31acc6731297834a334735f3b05e65e7
Padraic Slattery <pgoslatara@gmail.com>
2026-03-24 19:09:39 +0200

    Adding review comments

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {% if env_var("CI", "false") == "true" %} {{ env_var("DBT_SCHEMA") }}
+    {% if env_var("CI", "false") == "true" -%} {{ env_var("DBT_DATASET") }}
 
-    {% elif target.name == "prod" and env_var("CI", "false") == "false" %} {{ node.config.schema }}
+    {% elif target.name == "prod" %} {{ node.config.schema }}
 
     {% else %} {{ default__generate_schema_name(custom_schema_name, node) }}
 
